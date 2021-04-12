0b7a5beeb336476bbe0dad0acc3234cf2d75e109
Matthew Hill <matthewjh@users.noreply.github.com>
2021-04-12 16:34:10 +0100

    repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable, InjectionToken, Injector } from '@angular/core';
+
+const TOKEN = new InjectionToken<string>("string token", { providedIn: "root", factory: () => "from module"});
+const A_TOKEN = new InjectionToken<A>("A");
+
+@Injectable({providedIn: "root"})
+class A {
+  constructor (@Inject(TOKEN) tokenValue: string) {
+    console.log(`tokenValue in A = ${tokenValue}`);
+  }
+}
+
+
+@Injectable({providedIn: "root"})
+class AGetter {
+  constructor (private readonly injector: Injector) {}
+
+  getA () {
+    return this.injector.get(A);
+  }
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {provide: A_TOKEN, useFactory: (aGetter: AGetter) => aGetter.getA(), deps:[AGetter] },
+    {provide: TOKEN, useValue: "from component"}
+  ]
 })
 export class AppComponent {
   title = 'my-app';
+  constructor (@Inject(A_TOKEN) a: A) {
+  }
 }
